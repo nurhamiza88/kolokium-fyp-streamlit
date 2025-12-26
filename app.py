51dde51f3e83cb0f01492995b799b0d2db263ddd
nurhamiza88 <hamieza@fsmt.upsi.edu.my>
2025-12-26 17:41:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -186,3 +186,7 @@ if st.button("📤 Submit Penilaian"):
         st.error(f"❌ Gagal hantar data (Status: {r.status_code})")
 st.divider()
 st.caption("© 2026 | Sistem Penilaian Kolokium Projek Tahun Akhir | Dr. Nur Hamiza binti Adenan")
+st.image(
+    "Logo-Universiti-Pendidikan-Sultan-Idris-UPSI.png",
+    width=70
+)
