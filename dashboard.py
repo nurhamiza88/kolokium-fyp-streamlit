97c0f1258130ea992dc1c7aa01caba79e2ad9abd
nurhamiza88 <hamieza@fsmt.upsi.edu.my>
2025-12-29 08:46:28 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+st.set_page_config(
+    page_title="Dashboard Keputusan Kolokium FYP",
+    layout="wide"
+)
+
+st.title("📊 Dashboard Keputusan Kolokium Projek Tahun Akhir")
 
