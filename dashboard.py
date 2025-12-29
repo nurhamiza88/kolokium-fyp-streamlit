085caf0fa3fa747bff891874fc27bf27f9fae5f4
nurhamiza88 <hamieza@fsmt.upsi.edu.my>
2025-12-29 08:50:39 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,4 +8,18 @@ st.set_page_config(
 )
 
 st.title("📊 Dashboard Keputusan Kolokium Projek Tahun Akhir")
+# =====================
+# GOOGLE SHEET (RUMUSAN AKHIR)
+# =====================
+SHEET_ID = "14YrOAWbC0M4Cd2Vg02TEJ_xObOZaMuHEcDwpWM_cvfg"
+GID_RUMUSAN = "PASTE_GID_DI_SINI"
+
+CSV_URL = (
+    f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/"
+    f"export?format=csv&gid={GID_RUMUSAN}"
+)
+
+@st.cache_data
+def load_rumusan():
+    return pd.read_csv(CSV_URL)
 
