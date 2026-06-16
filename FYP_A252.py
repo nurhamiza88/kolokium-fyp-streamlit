2634d890d18a04194d466d664e90db1568077f3a
nurhamiza88 <hamieza@fsmt.upsi.edu.my>
2026-06-16 22:25:24 +0800

    Update FYP_A252.py

--- FYP_A252.py
+++ FYP_A252.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("📋 Sistem Penilaian Juri Kolokium Projek Tahun Akhir")
+st.title("📋 A252 - Sistem Penilaian Juri Kolokium Projek Tahun Akhir")
 
 # =====================
 # GOOGLE SHEET CSV
